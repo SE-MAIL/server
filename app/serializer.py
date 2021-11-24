de085bb3e16252b5dfdaf1d2ec99926058dcde8b
HyJun95 <82373254+HyJun95@users.noreply.github.com>
2021-11-25 01:58:16 +0900

    feat/ 뷰&URL까지 생성(추천 행동)

--- app/serializer.py
+++ app/serializer.py
@@ -1,14 +1,14 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from .models import Showerdataset
 from .models import Showerlog
 
-class ShowerlogSerializer(serializers.ModelSerializer):
+class ShowerlogSerializer(ModelSerializer):
     class Meta:
         model = Showerlog
-        fields = ('idshower', 'startTime', 'endTime', 'takenTime', 'data', 'user_id', 'emissions')
+        fields = ('idshower', 'startTime', 'endTime', 'takenTime', 'data', 'user_id', 'emissions') #'__all__'
 
-class ShowerdatasetSerializer(serializers.ModelSerializer):
+class ShowerdatasetSerializer(ModelSerializer):
     class Meta:
         model = Showerdataset
         fields = ('idshowerdataset', 'gender', 'age', 'averageemissions', 'count')
--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.response import Response
+from .models import Showerdataset
+from .models import Showerlog
+from .serializer import ShowerdatasetSerializer
+from .serializer import ShowerlogSerializer
 
 # Create your views here.
+
+class ShowerdatasetEmissionAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Showerdataset, pk=pk)
+
+    def get(self, request, pk, format=None):
+        Showerdataset = self.get_object(pk)
+        serializer = ShowerdatasetSerializer(Showerdataset)
+        return Response(serializer.data)
+
+class ShowerlogEmissionAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Showerlog, pk=pk)
+
+    def get(self, request, pk, format=None):
+        Showerlog = self.get_object(pk)
+        serializer = ShowerlogSerializer(Showerlog)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        Showerlog = self.get_object(pk)
+        serializer = ShowerlogSerializer(Showerlog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
