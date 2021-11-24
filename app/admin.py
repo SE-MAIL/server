b2f084e433132bce3cb59ea353ba052af9fd9906
gobin2 <bcgo9907@gmail.com>
2021-11-24 20:26:06 +0900

    feat/ admin 등록

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Showerdataset
+from .models import Showerlog
+from .models import Currentshowerdata
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Showerdataset)
+admin.site.register(Showerlog)
+admin.site.register(Currentshowerdata)
