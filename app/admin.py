7cd9f08394d02c612c8f12aeb93581d48236b273
HyJun95 <82373254+HyJun95@users.noreply.github.com>
2021-11-26 19:04:06 +0900

    feat / '현재' 테이블 이름 변경

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import User
 from .models import Showerdataset
 from .models import Showerlog
-from .models import Currentshowerdata
+from .models import Personalshowerdata
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Showerdataset)
 admin.site.register(Showerlog)
-admin.site.register(Currentshowerdata)
+admin.site.register(Personalshowerdata)
