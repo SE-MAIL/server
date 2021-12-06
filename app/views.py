051f24355c4800238a73706797effd4b598062e4
gobin2 <bcgo9907@gmail.com>
2021-12-07 03:30:41 +0900

    fix/ 주석 추가

--- app/views.py
+++ app/views.py
@@ -77,15 +77,15 @@ class PersonalShowerEmissionAPIView(APIView):
 
 class ActionShowerStartAPIView(APIView): # 시작할 때 받는거
     def getUser(self, first_name):
-        return get_object_or_404(AuthUser, first_name=first_name)
+        return get_object_or_404(AuthUser, first_name=first_name) # AuthUser가 User 테이블, first_name은 사용자의 이름
 
     def post(self, request, format=None): # 시작, 끝시간 체크
         try:
             starttime = timezone.now()+datetime.timedelta(hours=9)
-            first_name = request.data['action']['parameters']['user']['value']
-            user = self.getUser(first_name)
-            latestLog = Showerlog.objects.filter(auth_user=user).last()
-            showerlog = models.Showerlog(auth_user=user, starttime=starttime, sum=latestLog.sum)
+            first_name = request.data['action']['parameters']['user']['value'] # 누구의 요청에서 사용자의 이름 받기
+            user = self.getUser(first_name) # 사용자 이름으로 user 테이블에서 해당 사용자 데이터 불러오기
+            latestLog = Showerlog.objects.filter(auth_user=user).last() # 유저 데이터로 샤워로그 테이블 조회
+            showerlog = models.Showerlog(auth_user=user, starttime=starttime, sum=latestLog.sum) # sum은 직전 값을 불러옴.
             showerlog.save()
 
             response = {
@@ -111,24 +111,24 @@ class ActionShowerEndAPIView(APIView): # 끝날 때 받는거, 누구에서 '나
 
     def post(self, request, format=None):
         try:
-            endTime = timezone.now()+datetime.timedelta(hours=9)
-            first_name = request.data['action']['parameters']['user']['value']
-            user = self.getUser(first_name)
+            endTime = timezone.now()+datetime.timedelta(hours=9) # 요청 들어왔을 때 시간 기록
+            first_name = request.data['action']['parameters']['user']['value'] # 샤워시작과 동일 - 유저 데이터 불러오기
+            user = self.getUser(first_name) # 샤워시작과 동일
 
-            showerLogList = Showerlog.objects.all().filter(auth_user=user)
-            latestLog = showerLogList.last()
-            logging.warn(latestLog)
-            beforeLastestLog = showerLogList.order_by('-idshower')[1]
-            logging.warn(beforeLastestLog)
+            showerLogList = Showerlog.objects.all().filter(auth_user=user) # 유저 데이터로 샤워로그 기록 조회. 해당 유저의 샤워로그 전체를 불러옴.
+            latestLog = showerLogList.last() # 해당 유저의 전체 로그 중 마지막
+            beforeLastestLog = showerLogList.order_by('-idshower')[1] # 해당 유저의 전체 로그 중 마지막에서 두번째 값.
+            # order_by('조건')에서 조건 앞에 -를 붙이면 역순으로 정렬함(내림차순). -> [0]번이 가장 마지막 값(==last()) [1]이 마지막에서 두 번째 값.
 
-            startTime = latestLog.starttime
-            takenTime = (endTime - startTime).total_seconds()
-            latestLog.takentime = takenTime
-            emissions = takenTime*2
-            latestLog.emissions = emissions
-            if str(beforeLastestLog.starttime)[0:7] != str(endTime)[0:7] :
-                logging.warn(str(beforeLastestLog.starttime)[0:7])
-                logging.warn(str(endTime)[0:7])
+            startTime = latestLog.starttime # 가장 마지막 로그에서 시작시간 불러옴.
+            takenTime = (endTime - startTime).total_seconds() # 종료시간 - 시작시간 = 총 소요시간
+            # total_seconds()는 datetime type을 초 단위로 변환해줌.
+            latestLog.takentime = takenTime # 가장 마지막 로그에 소요시간 저장
+            emissions = takenTime*2 # 샤워 10분당 1080g의 탄소배출 -> 1초당 약 1.8g 배출 => 약 2g배출로 계산.
+            
+            latestLog.emissions = emissions # 배출량 저장.
+            if str(beforeLastestLog.starttime)[0:7] != str(endTime)[0:7] : # 마지막에서 두번째값의 일자와 현재 일자를 비교
+                # latestLog(가장 마지막 row)는 현재 샤워를 시작했을 때 기록된 row다. 따라서 이전 샤워 데이터를 불러오려면 마지막에서 두 번째 값을 불러와야 함.
                 latestLog.sum == 0
             latestLog.sum = latestLog.sum + emissions
             latestLog.endtime=endTime
@@ -149,7 +149,7 @@ class ActionShowerEndAPIView(APIView): # 끝날 때 받는거, 누구에서 '나
             )
 
 
-class TestAPIView(APIView):
+class TestAPIView(APIView): # 로그인 기능 토큰 확인용 테스트 뷰
     permission_classes = (IsAuthenticated,)
     def get(self, request):
         token = request.META.get('HTTP_AUTHORIZATION')
